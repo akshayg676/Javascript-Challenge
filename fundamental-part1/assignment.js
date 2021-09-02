6866df0d501a4d3f25c0b67e44e361b07847dda1
Akshay G <akshayg676@gmail.com>
2021-09-02 20:42:47 +0530

    assigment3 complete

--- fundamental-part1/assignment.js
+++ fundamental-part1/assignment.js
@@ -1,12 +1,28 @@
 const country = "India";
 const continent = "Asia";
-let population = 3;
+let population = 4;
+population++;
+const finlandPopulation = 6;
+const averagePopulation = 33;
+let countryFirstHalf = 2;
+let countrySecondHalf = 2;
 const isIsland = false;
 let language;
+language = "Hindi";
+const description =
+  country +
+  " is in " +
+  continent +
+  " , and its " +
+  population +
+  " million people speak " +
+  language;
 
-console.log(typeof country);
-console.log(typeof population);
-console.log(typeof isIsland);
-console.log(typeof language);
-
-console.log(country, continent, population);
+// console.log(typeof country);
+// console.log(typeof population);
+// console.log(typeof isIsland);
+// console.log(typeof language);
+// console.log(country, continent, population);
+console.log(population > finlandPopulation);
+console.log(population < averagePopulation);
+console.log(description);
