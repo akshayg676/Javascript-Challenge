159fc03fa7e0d8e29c4c94530fb86d270d522f07
Akshay G <akshayg676@gmail.com>
2021-09-02 20:13:27 +0530

    assigment2 complete

--- fundamental-part1/assignment.js
+++ fundamental-part1/assignment.js
@@ -1,5 +1,12 @@
 let country = "India";
 let continent = "Asia";
-let population = 3 + " million";
+let population = 3;
+let isIsland = false;
+let language;
+
+console.log(typeof country);
+console.log(typeof population);
+console.log(typeof isIsland);
+console.log(typeof language);
 
 console.log(country, continent, population);
