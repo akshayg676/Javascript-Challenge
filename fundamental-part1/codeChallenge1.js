87a0c6f734dec19dc607d329cb65030bc2f0ce5a
Akshay G <akshayg676@gmail.com>
2021-09-04 13:44:12 +0530

    complete codechallenge1

---
+++ fundamental-part1/codeChallenge1.js
@@ -0,0 +1,10 @@
+const markHeight = 1.69;
+const markWeight = 78;
+const johnHeight = 1.95;
+const johnWeight = 92;
+
+const markBMI = markWeight / markHeight ** 2;
+const johnBMI = johnWeight / johnHeight ** 2;
+
+let markHeigherBMI = markBMI > johnBMI;
+console.log(markBMI, johnBMI, markHeigherBMI);
