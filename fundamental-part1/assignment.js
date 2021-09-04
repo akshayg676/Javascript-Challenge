2d9aad2a80ec045f4c8183f82d3af62cf3bdb964
Akshay G <akshayg676@gmail.com>
2021-09-04 20:46:41 +0530

    conversion and coercion complete

--- fundamental-part1/assignment.js
+++ fundamental-part1/assignment.js
@@ -21,10 +21,16 @@ const description = `${country} is in ${continent}, and it's ${population} milli
 // console.log(population < averagePopulation);
 // console.log(description);
 
-if (population > 33) {
-  console.log(`${country}'s population is above average`);
-} else {
-  console.log(
-    `${country}'s population is  ${33 - population} million below average`
-  );
-}
+// if (population > 33) {
+//   console.log(`${country}'s population is above average`);
+// } else {
+//   console.log(
+//     `${country}'s population is  ${33 - population} million below average`
+//   );
+// }
+
+// "9" - "5"; // 4
+// "19" - "13" + "17"; //617
+// "19" - "13" + 17; //23
+// "123" < 57; //false
+// 5 + 6 + "4" + 9 - 4 - 2; //1149
--- fundamental-part1/codeChallenge1.js
+++ fundamental-part1/codeChallenge1.js
@@ -6,8 +6,8 @@ const johnWeight = 92;
 const markBMI = markWeight / markHeight ** 2;
 const johnBMI = johnWeight / johnHeight ** 2;
 
-if (markBMI > johnBMI) {
-  console.log(`mark's BMI ${markBMI} is higher than john's ${johnBMI}`);
-} else {
-  console.log(`john's BMI ${johnBMI} is higher than mark's ${markBMI}`);
-}
+// if (markBMI > johnBMI) {
+//   console.log(`mark's BMI ${markBMI} is higher than john's ${johnBMI}`);
+// } else {
+//   console.log(`john's BMI ${johnBMI} is higher than mark's ${markBMI}`);
+// }
