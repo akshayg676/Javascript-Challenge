564ea483a77406b4d69440d75fdc633b7ebf7437
Akshay G <akshayg676@gmail.com>
2021-09-04 20:17:36 +0530

    codeChallenge2 complete

--- fundamental-part1/codeChallenge1.js
+++ fundamental-part1/codeChallenge1.js
@@ -6,5 +6,8 @@ const johnWeight = 92;
 const markBMI = markWeight / markHeight ** 2;
 const johnBMI = johnWeight / johnHeight ** 2;
 
-let markHeigherBMI = markBMI > johnBMI;
-console.log(markBMI, johnBMI, markHeigherBMI);
+if (markBMI > johnBMI) {
+  console.log("mark's BMI is higher than john's");
+} else {
+  console.log("john's BMI is higher than mark's");
+}
